436fafd72b8d50f640334ce953c2cbeda611699c
zachaweke47 <43449696+zachaweke47@users.noreply.github.com>
2018-11-07 21:44:42 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+var num = prompt("Enter Number from 0-10", "")
+
+var table = num;
+var operator = 'multiple'; // Type of calculation
+var i = 1;                 // Set counter to 1
+var msg = '<h2>Multiplication Table</h2>';              // Message
+
+if (operator === 'Addition') {
+  // Do addition
+  while (i < 11) {
+    msg += i + ' + ' + table + ' = ' + (i + table) + '<br />';
+    i++;
+  }
+} else {
+  // Do multiplication
+  while (i < 11) {
+    msg += i + ' x ' + table + ' = ' + (i * table) + '<br />';
+    i++;
+  }
+}
+
+// Write the message into the page
+var el = document.getElementById('blackboard');
+el.innerHTML = msg; 
